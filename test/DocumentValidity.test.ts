f04a1d2f0c346e49674473933afdc70d438eee3c
Jay Lee <jaylee.possible@gmail.com>
2021-10-04 19:24:06 +0900

    test code updated

---
+++ test/DocumentValidity.test.ts
@@ -0,0 +1,88 @@
+/**
+ * @jest-environment jsdom
+ */
+
+import * as fs from 'fs'
+import * as path from 'path'
+
+import posts, { IPost } from '../src/config/post.config'
+
+import marked from 'marked'
+import { organizeHeadings } from '../src/components/ContentIndexer/Heading'
+
+describe(`Document validity test`, () => {
+  test(`Document shouldn't have same title`, () => {
+    const postTitles: Set<string> = new Set<string>(
+      posts.map((post: IPost) => post.title)
+    )
+    expect(postTitles.size).toEqual(posts.length)
+  })
+
+  test(`Markdown files should exist`, () => {
+    const isMarkdownExists: boolean = posts.every((post: IPost) => {
+      const markdownPath: string = path.resolve(
+        __dirname,
+        `../public/resources/${post.category}/${post.fileName}`
+      )
+      return fs.existsSync(markdownPath)
+    })
+
+    expect(isMarkdownExists).toEqual(true)
+  })
+
+  test(`Thumbnail files should exist`, () => {
+    const isThumbnailExists: boolean = posts.every((post: IPost) => {
+      if (!post.thumbnailName) return true
+      const markdownPath: string = path.resolve(
+        __dirname,
+        `../public/resources/assets/${post.thumbnailName}`
+      )
+      return fs.existsSync(markdownPath)
+    })
+
+    expect(isThumbnailExists).toEqual(true)
+  })
+
+  test(`Every post should have its description`, () => {
+    const hasDescription: boolean = posts.every(
+      (post: IPost) => post.description
+    )
+
+    expect(hasDescription).toEqual(true)
+  })
+
+  test(`Every post should have at least one tag`, () => {
+    const isPostHasTag: boolean = posts.every(
+      (post: IPost) => post.tags.length >= 1
+    )
+
+    expect(isPostHasTag).toEqual(true)
+  })
+
+  test(`Every post has right structure of heading elements`, async () => {
+    let validFlag: boolean = true
+
+    for (let i = 0; i < posts.length; i++) {
+      const post: IPost = posts[i]
+      try {
+        const content: string = marked(
+          fs.readFileSync(
+            path.resolve(
+              __dirname,
+              `../public/resources/${post.category}/${post.fileName}`
+            ),
+            'utf8'
+          )
+        )
+
+        organizeHeadings(content)
+      } catch (e) {
+        validFlag = false
+        console.log(`Invalid structure of heading is found in ${post.fileName}`)
+        throw e
+      }
+    }
+
+    expect(validFlag).toEqual(true)
+  })
+})
--- test/sample.test.ts
+++
@@ -1,7 +0,0 @@
-export {}
-
-describe('Sample test', () => {
-  it('Should be equal to 2', () => {
-    expect(1 + 1).toEqual(2)
-  })
-})
