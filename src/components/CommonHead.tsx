38fc9a2c604573e090dd66476475d940a7f3f49e
Jay Lee <jaylee.possible@gmail.com>
2021-10-05 11:06:31 +0900

    Remove HeadMeta

--- src/components/CommonHead.tsx
+++ src/components/CommonHead.tsx
@@ -3,9 +3,8 @@ import React from 'react'
 import Head from 'next/head'
 import { NextRouter, useRouter } from 'next/router'
 
-import { blogConfig } from '../config/blog.config'
+import { blogConfig, IBlogConfig } from '../config/blog.config'
 import menuConfigs, { IMenuConfig } from '../config/menu.config'
-import HeadMeta from './HeadMeta'
 
 export type CommonHeadProps = {
   title?: string
@@ -19,28 +18,50 @@ const CommonHead = (props: CommonHeadProps): JSX.Element => {
     (menuConfig: IMenuConfig) => menuConfig.path === router.route
   )
 
-  const title: string = (
+  const title: string = computeTitle(props, blogConfig, menuConfig)
+
+  const description: string | undefined =
+    props.description || menuConfig?.description
+  const keywords: string[] | undefined = props.keywords
+  const author: string | undefined = blogConfig.author || blogConfig.email
+
+  return (
+    <Head>
+      <title>{title}</title>
+
+      <meta property="og:title" content={title} />
+      {description ? (
+        <>
+          <meta property="og:description" content={description} />
+          <meta name="description" content={description} />
+        </>
+      ) : (
+        ''
+      )}
+
+      {keywords?.length ? (
+        <meta name="keyword" content={keywords.join(', ')} />
+      ) : (
+        ''
+      )}
+
+      {author ? <meta name="author" content={author} /> : ''}
+    </Head>
+  )
+}
+
+function computeTitle(
+  props: CommonHeadProps,
+  blogConfig: IBlogConfig,
+  menuConfig?: IMenuConfig
+): string {
+  return (
     props.title
       ? [blogConfig.title, props.title]
       : menuConfig?.title
       ? [blogConfig.title, menuConfig.title]
       : [blogConfig.title]
   ).join(` ${blogConfig.titleDelimiter || '|'} `)
-  const description: string | undefined = menuConfig?.description
-  const keywords: string[] | undefined = props.keywords
-  const author: string | undefined = blogConfig.author || blogConfig.email
-
-  return (
-    <Head>
-      <HeadMeta
-        title={title}
-        description={description}
-        keywords={keywords}
-        author={author}
-      />
-      <title>{title}</title>
-    </Head>
-  )
 }
 
 export default CommonHead
