04a835a9e8509129336b4f107e73a07732a97f91
Jay Lee <jaylee.possible@gmail.com>
2021-10-15 02:44:54 +0900

    Update title - #26

--- src/components/BlogTitle.tsx
+++ src/components/BlogTitle.tsx
@@ -1,14 +1,14 @@
 import { blogConfig } from '../config/blog.config'
 import styled from 'styled-components'
 
-const StyledTitle = styled.h1`
+const StyledTitle = styled.p`
   text-align: center;
 `
 
 const BlogTitle = (): JSX.Element => {
   return (
     <>
-      <StyledTitle>{blogConfig.title}</StyledTitle>
+      <StyledTitle className="title">{blogConfig.title}</StyledTitle>
     </>
   )
 }
